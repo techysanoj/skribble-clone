5c2f5ed6c908ed689b69456bb26447c9e3f7ff16
Sanoj <96434101+techysanoj@users.noreply.github.com>
2024-06-07 23:56:35 +0530

    updating required file

--- frontend/src/views/HomeScreen.jsx
+++ frontend/src/views/HomeScreen.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import "../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faEnvelope,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
 
 function HomeScreen() {
   return (
-    <div>HomeScreen</div>
+    <div>
+      <header className="app-header">
+        <h1>skribblay.you</h1>
+      </header>
+      <div className="main-container">
+        <div className="input-container">
+          <input type="text" placeholder="Enter your Name" className="input" />
+          <select className="input">
+            <option value="en">English</option>
+            <option value="fr">French</option>
+            <option value="es">Spanish</option>
+            <option value="de">German</option>
+            <option value="it">Italian</option>
+          </select>
+        </div>
+        <div className="avatar-container">
+          <button className="arrow-button">←</button>
+          <img src="avatar-2.png" alt="Avatar" className="avatar" ></img>
+          <button className="arrow-button">→</button>
+        </div>
+        <input
+          type="text"
+          placeholder="Enter room code Here"
+          className="room-code-input"
+        />
+        <button className="play-button">Play !</button>
+      </div>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faHome} className="icon" />
+        <FontAwesomeIcon icon={faEnvelope} className="icon" />
+        <FontAwesomeIcon icon={faQuestionCircle} className="icon" />
+      </div>
+    </div>
   )
 }
 
