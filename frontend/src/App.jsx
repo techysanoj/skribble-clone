2707402b4c0fb591ff3508c64dbffbd00ee10aa2
Shashwat Aggarwal <shashwatagg04@gmail.com>
2024-06-07 23:08:33 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomeScreen from "./views/HomeScreen";
+import PlayScreen from "./views/PlayScreen";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route
+          path="/"
+          element={<HomeScreen />}
+        ></Route>
+        <Route
+          path="/play"
+          element={<PlayScreen />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
